5950c579124a70ad732bdb272f4389ae7e0e0cb4
Guilherme <80716667+Siluryan@users.noreply.github.com>
2022-04-03 21:39:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -22,6 +22,10 @@ def find_post(id):
         if p['id'] == id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get('/')
 def root():
@@ -33,8 +37,8 @@ def app_get():
     return {'Data': my_posts}
 
 
-@app.post('/posts')
-def app_post(post: Post):
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000) 
     my_posts.append(post_dict)
@@ -42,7 +46,17 @@ def app_post(post: Post):
 
 
 @app.get('/posts/{id}')
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
-    print(post)
+    if not post:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"Id:{id} not found ")
     return {'Post detail':post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Id:{id} does not exist')
+    my_posts.pop(index)
+    return Response (status_code=status.HTTP_204_NO_CONTENT)
