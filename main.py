608125cd9af85eba182d972e8b388ad372604db4
Guilherme <80716667+Siluryan@users.noreply.github.com>
2022-04-05 23:26:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,28 @@ from random import randrange
 
 
 app = FastAPI()
-my_posts = list()
+my_posts = [{"title":"title 0", "content":"content 0", "published": False, "id": 0}]
 
 
 class Post(BaseModel):
     title: str
     content: str
-    published: bool = True
-    rating: Optional[int] = None
+    published: bool = False   
 
 
 def find_post(id):
     for p in my_posts:
         if p['id'] == id:
-            return p
+           return p
 
-def find_index_post(id):
+def find_index(id):
     for i, p in enumerate(my_posts):
         if p['id'] == id:
             return i
+
+def msg_404(id):
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Id:{id} does not exist')
+    
 
 @app.get('/')
 def root():
@@ -48,14 +51,25 @@ def create_post(post: Post):
 def get_post(id: int, response: Response):
     post = find_post(id)
     if not post:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"Id:{id} not found ")
+         msg_404(id)
     return {'Post detail':post}
 
 
-@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
+    index = find_index(id)
     if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Id:{id} does not exist')
+       msg_404(id)
     my_posts.pop(index)
     return Response (status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put('posts/{id}')
+def update_post(id: int, post: Post):
+    index = find_index(id)
+    if index == None:
+       msg_404(id)
+    post_dict = post.dict()    
+    post_dict['id'] = id
+    my_post[index] = post_dict
+    return {"data":post_dict}
